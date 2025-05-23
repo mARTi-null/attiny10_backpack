076cc98d5c6b4ce52e7147ed2eb292159954b1cb
cazmaTronik <cyberzivac@gmail.com>
2025-05-23 19:13:47 +0200

    xD

--- attiny_10ddb/attiny10_ddb.asm
+++ attiny_10ddb/attiny10_ddb.asm
@@ -1,16 +1,16 @@
-/*
-Double Dabble algorithm for Attiny10 written in assembly by: _cazmaTronik
+/***********************************************************************************************************************
+Double Dabble algorithm for Attiny10 written in assembly
 
 Created: 28.10.2024.
-Author : _cazmaTronik
+Author : mARTi_null
 Microcontroller: Attiny10
 IDE: Microchip Studio 7.0
 
 This function uses Double Dabble algorithm to convert binary value from ADCL register into
 binary coded decimal (BCD) notation. The main purpose of this conversion is for displaying
 read out values from ADC to OLED display like SSD1306 or 7 Segment display.
-The whole function is written in avr assembler and takes only 46 bytes of program memory.
-Function takes around 15 Âµs to complete with 8MHz clock on Attiny10.
+The whole function is written in avr assembler and takes only 46 bytes of program memory when compiled
+Function takes around 15 µs to complete with 8MHz clock on Attiny10.
 
 ;-------------------------------------------------------------
 !---CONVERTED DIGITS ARE RETURNED IN REGISTERS R18 and R19---!
@@ -22,16 +22,18 @@ Decimal number example:
 	HEX = 7B
 	DEC = 123 = "1" is stored in R19, "2" is stored in R18 high nibble "3" is stored in R18 low nibble
 
-;---------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------
 Example of extracting individual digits from R18:
 	mov		Rn, R18				; Load data from R18 into Rn
 	andi	R18, 0b0000 1111	; Make all bits in high nibble of R18 0 by AND-ing them with 0
 	andi	Rn, 0b1111 0000		; Make all bits in low nibble of Rn 0 by AND-ing them with 0
-	swap	Rn					; Swap Rn high and low nibble places to have all individual digits in lower nibble or R19, Rn and R18
-;---------------------------------------------------------------------------------------------------------------------------------------
+	swap	Rn					; Swap Rn high and low nibble places to have all individual digits
+								  in lower nibble or R19, Rn and R18
+------------------------------------------------------------------------------------------------------------------------
 
 Source: https://en.wikipedia.org/wiki/Double_dabble.
-*/
+
+***********************************************************************************************************************/
 
 ;-----Register definitions-----;
 .def	byte = r16
@@ -46,11 +48,11 @@ Source: https://en.wikipedia.org/wiki/Double_dabble.
 ;-----DDB function-----;
 ddb:
 	in		byte, ADCL			; Load ADC value for conversion
-	;ldi	byte, 0xFF			; For testing purpose, byte for conversion to decimal
+	;ldi		byte, 0xFF			; For testing purpose, byte for conversion to decimal
 	ldi		counter, 0x08		; Set counter to 8 (8 bits)
 	ldi		dig_0_1, 0x00		; Low nibble of dig_0_1 will be digit 0, high nibble will be digit 1
 	ldi		dig_2, 0x00			; Low nibble of dig_2 will be digit 2
-	ldi		add_low, 0b00000011		; Value of 3 for low nibble addition
+	ldi		add_low, 0b00000011			; Value of 3 for low nibble addition
 	ldi		add_high, 0b00110000		; Value of 3 for high nibble addition
 shift_data:
 	lsl		byte		; Shift bits to the left and set Carry Flag if bit shifted out is 1
@@ -63,13 +65,13 @@ shift_data:
 lo_nibble:
 	andi	compare_low, 0x0F		; Isolate only low nibble
 	cpi		compare_low, 0x05		; Compare low nibble with 5
-	brlo	hi_nibble			; If low nibble is lower than 5 skip addition and check high nibble
+	brlo	hi_nibble				; If low nibble is lower than 5 skip addition and check high nibble
 	add		dig_0_1, add_low		; Add 3 to low nibble if it is equal or bigger than 5
 hi_nibble:
 	andi	compare_high, 0xF0		; Isolate high nibble
 	cpi		compare_high, 0x50		; Compare high nibble with 5
-	brlo	shift_data			; If high nibble is lower than 5 skipp addition and start next bit shift
+	brlo	shift_data				; If high nibble is lower than 5 skipp addition and start next bit shift
 	add		dig_0_1, add_high		; Add 3 to high nibble if it is equal or bigger than 5
-	rjmp	shift_data			; Start next bit shift
+	rjmp	shift_data	; Start next bit shift
 end_ddb:
 ret		; Return from function
